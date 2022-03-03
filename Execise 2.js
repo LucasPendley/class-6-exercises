8e05eb50b78fa2adb65c8aeeda400e5c60a98a48
LucasPendley <99683560+LucasPendley@users.noreply.github.com>
2022-03-03 14:54:49 -0600

    Class 6 exercises

---
+++ Execise 2.js
@@ -0,0 +1,9 @@
+//Execise 2
+
+const listOne = reversibleInclusiveList(1, 5)    //➞    [1, 2, 3, 4, 5]
+
+const listTwo = reversibleInclusiveList(2, 8)    //➞    [2, 3, 4, 5, 6, 7, 8]
+
+const listThree = reversibleInclusiveList(10, 20)    //➞   [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+const listFour = reversibleInclusiveList(24, 17)    //➞   [24, 23, 22, 21, 20, 19, 18, 17]
---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+// Exercise 1
+
+const baseValue = prompt('20');
+const heightValue = prompt('10');
+    
+const areaValue = (baseValue * heightValue) / 2;
+    
+console.log(
+`The area of the triangle is ${areaValue}`
+)
